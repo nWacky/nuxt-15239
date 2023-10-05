c95107ae3ab9dc13466ae5077ce15a2db78c33ef
nWacky <gosha.evtushenko@gmail.com>
2023-10-05 12:06:09 +0300

    remove router still leaks

--- app.vue
+++ app.vue
@@ -1,8 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const showItems = ref(false);
+
+const data = useGetFirstDataSynchronous();
+</script>
 
 <template>
   <div>
     <!-- <NuxtPage /> -->
-    <RouterView />
+    <!-- <RouterView /> -->
+
+    <div>
+      <h1>app.vue</h1>
+
+      <br />
+      <button @click="showItems = !showItems">
+        Toggle show to {{ !showItems }}
+      </button>
+      <br />
+
+      <div v-if="showItems">
+        <TheList :items="data?.items ?? []" />
+      </div>
+    </div>
   </div>
 </template>
